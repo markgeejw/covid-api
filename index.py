925a17c346dad5c007e809ba99717f10ed4e5ede
Mark Gee <jw.ziggee@gmail.com>
2020-03-29 15:30:16 +0800

    add root page

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import logging
 import sys
 
 import numpy as np
-from flask import Flask, request
+from flask import Flask, request, render_template, make_response
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
@@ -27,6 +27,11 @@ if logging.root.handlers == []:
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+class Root(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'),200,headers)
 
 class Model(Resource):
     def get(self):
@@ -159,6 +164,7 @@ class StateCases(Resource):
             logger.debug(e)
             return 'Error', 400
 
+api.add_resource(Root, '/')
 api.add_resource(Model, '/model')
 api.add_resource(StateInfo, '/info')
 api.add_resource(StateCases, '/case')
